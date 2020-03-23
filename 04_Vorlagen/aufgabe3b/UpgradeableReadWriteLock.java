f6208a43e21c1cf492571bc29e1cad279844f7c2
Fabian Germann <fabian.germann@hsr.ch>
2020-03-23 15:34:04 +0100

    3b

--- 04_Vorlagen/aufgabe3b/UpgradeableReadWriteLock.java
+++ 04_Vorlagen/aufgabe3b/UpgradeableReadWriteLock.java
@@ -1,29 +1,60 @@
 package aufgabe3b;
 
+import java.util.concurrent.Semaphore;
+
 public class UpgradeableReadWriteLock {
-	// TODO: Implement
+    private int readLocks = 0;
+    private boolean upgradeableReadLock = false;
+    private boolean writeLock = false;
+    private Thread upgradeThread;
 
-	public void readLock() throws InterruptedException {
-		// TODO:
-	}
+    public synchronized void readLock() throws InterruptedException {
+        while (writeLock) {
+            wait();
+        }
+        readLocks++;
+        notifyAll();
+    }
 
-	public void readUnlock() {
-		// TODO:
-	}
+    public synchronized void readUnlock() {
+        readLocks--;
+        notifyAll();
+    }
 
-	public void upgradeableReadLock() throws InterruptedException {
-		// TODO:
-	}
+    public synchronized void upgradeableReadLock() throws InterruptedException {
+        while (upgradeableReadLock || writeLock) {
+            wait();
+        }
+        upgradeableReadLock = true;
+        upgradeThread = Thread.currentThread();
+    }
 
-	public void upgradeableReadUnlock() {
-		// TODO:
-	}
+    public synchronized void upgradeableReadUnlock() {
+        upgradeableReadLock = false;
+        notifyAll();
+    }
 
-	public void writeLock() throws InterruptedException {
-		// TODO:
-	}
+    public synchronized void writeLock() throws InterruptedException {
+        while (readLocks > 0 || upgradeableReadLock || writeLock) {
+            if (upgradeThread == Thread.currentThread()) {
+                // Upgrade
+                upgradeableReadLock = false;
+                break;
+            }
+            wait();
+        }
+        writeLock = true;
+    }
 
-	public void writeUnlock() {
-		// TODO:
-	}
+    public synchronized void writeUnlock() {
+        writeLock = false;
+        notifyAll();
+    }
 }
+
+/*
+3a)
+Die Unterscheidung ist notwendig, da mehrere  "upgradeableReadLock()" Locks nicht kompatibel sind.
+Diese Information kann man aus der Kompatibilitätsmatrix entnehmen.
+Hätte man 2 "upgradeableReadLock()" Locks, welche das Upgrade auf den WriteLock() durchführen wollten, würden sich diese gegenseitig blockieren, da WriteLocks exklusiv sind.
+ */
